6630ae0d6e9a231a2f119b7c3370a63544e0e31a
Drennann <delpan.drennan@gmail.com>
2023-05-04 21:02:14 -0300

    [ADD] Card Mobile view

--- frontend/src/components/PreviousEvents.tsx
+++ frontend/src/components/PreviousEvents.tsx
@@ -6,7 +6,7 @@ export default function PreviousEvents() {
   return (
     <div>
       <h2 className='m-6 font-bold text-2xl'>Images</h2>
-      <div className='grid-cols-5 grid overflow-x-hidden'>
+      <div className='md:grid-cols-5 grid overflow-x-hidden gap-4 md:gap-0'>
         {new Array(TOTAL_IMAGES).fill(0).map((_, index) => (
           <SquaredCard key={index} />
         ))}
--- frontend/src/components/SquaredCard.tsx
+++ frontend/src/components/SquaredCard.tsx
@@ -1,6 +1,6 @@
 export default function SquaredCard() {
   return (
-    <div className='w-[20vw] h-[20vw] relative'>
+    <div className='m-auto w-[min(100vw,360px)] h-[min(100vw,360px)] md:w-[20vw] md:h-[20vw] relative'>
       <div className='w-full h-full bg-[url("/social.jpg")] absolute bg-center bg-cover' />
       <div className='w-full h-full absolute bg-[#000] opacity-60 hover:opacity-0 z-10 transition-opacity duration-300' />
       <div className='absolute w-full h-full'>
